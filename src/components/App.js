858a2168aa9fcc6136cb550c3fb75f71611d9955
Heather <hlmathies@gmail.com>
2019-09-21 09:17:45 -0400

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import '../App.css';
+import Particle from "./Particle";
+
+class App extends Component {
+  render() {
+    return <Particle/>;
+  }
+}
+
+export default App;
